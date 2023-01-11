12311542749fe124202239413ce127e3352683d0
Chirak <Pyc2002@yandex.ru>
2023-01-11 19:01:30 +0300

    controller.py  added

---
+++ controller.py
@@ -0,0 +1,92 @@
+
+import logger
+import csv
+import os
+import constants
+from constants import DATA_BASE
+import re
+
+
+
+
+def fieldnames():
+    """Create file.csv if it not exists, to avoid duplicate headers
+    
+    args -> None
+    returns -> None
+    """
+    if not os.path.exists(DATA_BASE): 
+        with open(DATA_BASE, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file, dialect='excel', delimiter='|')
+            writer.writerow(['Имя', 'Фамилия', 'Телефон', 'Группа', 'Комментарий'])
+            file.close()
+
+
+def add_contact():
+    """ Adding data of new contact in csv-file. User inputs data (str), def create dict and add it in file.
+    args -> None
+    returns -> None
+    """
+    fieldnames()
+    new_contact = {'Имя': constants.NAME.title(), 'Фамилия': constants.LAST_NAME.title(), 'Телефон': constants.PHONE_NUM, 'Группа': constants.GROUP, 'Комментарий': constants.COMMENT}
+    logger.write(new_contact)
+    
+
+    with open(DATA_BASE, mode='a', newline='', encoding='utf-8') as csv_file:
+        field_names = ['Имя', 'Фамилия', 'Телефон', 'Группа', 'Комментарий']
+
+        writer = csv.DictWriter(csv_file, fieldnames=field_names, dialect='excel', restval='', delimiter='|')
+        writer.writerow(new_contact)
+        csv_file.close()
+
+
+
+def Search_contact(update):
+    """ Searching contact in csv-file. User inputs data (str), def create string with results.
+    args -> None
+    returns -> str
+    """
+    data = update.message.text
+    # data = input(' ВВендите имя\n')
+    result = ''
+   
+    with open(DATA_BASE, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='|')
+        for item in reader:
+            
+            if data.title() in item['Имя'] or data.title() in item['Фамилия'] or data.title() in item['Группа']:
+                
+                res = item['Имя']+ ' ' + item['Фамилия']+ ' ' + item['Телефон']+ ' ' + item['Группа']+ ' ' + item['Комментарий']
+                result += res + '\n'
+    
+        return result
+    
+            
+def Delete_contact(update):
+    """ Removing contact in csv-file. User inputs data (str), def create new file without contact, removed old file and renemed new.
+    args -> None
+    returns -> None
+    """
+    data = update.message.text
+    # data = input(' ВВендите номер\n')
+    with open(DATA_BASE, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='|')
+        with open('temp.csv', mode='w', encoding='utf-8', newline='') as temp_file:
+            writer = csv.DictWriter(temp_file, fieldnames=reader.fieldnames, dialect='excel', restval='', delimiter='|')
+            writer.writeheader()
+            writer.writerows(filter(lambda x: x.get('Телефон') not in data, reader))
+    logger.write_del(data)
+    os.remove(DATA_BASE)
+    os.rename('temp.csv',DATA_BASE)
+    
+    
+
+# def Print():
+#     # print(Delete_contact())
+#     print(Search_contact())
+
+# Print()
+
+
+
+
